85187b818f541d63539b5ca0230c53d60fba7f8c
Pankaj Priscilla <pankajpriscilla@gmail.com>
2021-01-12 08:46:42 -0500

    defaults added

--- test/authUtil.test.js
+++ test/authUtil.test.js
@@ -3,11 +3,11 @@ const assert = require('assert').strict;
 
 
 describe('AuthUtil test', function () {
-    const authUtil = new AuthUtil();
+    const authUtil = new AuthUtil('sha512', 'dev-secret', 'base64');
 
     describe('Create random salt', function () {
         it('creates a random salt string', function (done) {
-            const salt = authUtil.createRandomSalt();
+            const salt = authUtil.createRandomSalt(new Date().valueOf().toString());
             assert.ok(salt, 'Salt is not created');
             done();
         });
