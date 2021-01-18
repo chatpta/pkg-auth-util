70598d391bb7cafe73c6c8c356ed8508f7bf1001
Pankaj Priscilla <pankajpriscilla@gmail.com>
2021-01-18 06:27:30 -0500

    duplicate removed

--- test/util/hash.test.js
+++ test/util/hash.test.js
@@ -15,40 +15,44 @@ describe('Hash test', function () {
     });
 
     describe('createHmacString', function () {
-        it('creates hmac string', async function () {
-            const salt = await auth.createRandomSalt();
-            const hmac = await auth.createHmacString(salt);
+        it('creates hmac string',  function (done) {
+            const salt = auth.createRandomSalt();
+            const hmac = auth.createHmacString(salt);
             assert.ok(hmac.length > 39);
+            done();
         });
     });
 
     describe('createPasswordHashStoreString', function () {
-        it('creates hash string to store', async function () {
+        it('creates hash string to store', function (done) {
             const password = "my-secret-password";
-            const salt = await auth.createRandomSalt();
-            const storeHashString = await auth.createPasswordHashStoreString(password, salt);
+            const salt = auth.createRandomSalt();
+            const storeHashString = auth.createPasswordHashStoreString(password, salt);
             assert.ok(storeHashString.length > 139);
+            done();
         });
     });
 
     describe('verifyPasswordHash good input', function () {
-        it('verifies hash against string to store', async function () {
+        it('verifies hash against string to store', function (done) {
             const password = "my-secret-password";
-            const salt = await auth.createRandomSalt();
+            const salt = auth.createRandomSalt();
             const storeHashString = auth.createPasswordHashStoreString(password, salt);
             const verified = auth.verifyPasswordHash(password, storeHashString);
             assert.ok(verified);
+            done();
         });
     });
 
     describe('verifyPasswordHash bad input', function () {
-        it('verifies hash against string to store', async function () {
+        it('verifies hash against string to store', function (done) {
             const password = "my-secret-password";
             const badPassword = "my-bad-password";
-            const salt = await auth.createRandomSalt();
-            const storeHashString = await auth.createPasswordHashStoreString(password, salt);
-            const verified = await auth.verifyPasswordHash(badPassword, storeHashString);
+            const salt = auth.createRandomSalt();
+            const storeHashString = auth.createPasswordHashStoreString(password, salt);
+            const verified = auth.verifyPasswordHash(badPassword, storeHashString);
             assert.ok(!verified);
+            done();
         });
     });
 
