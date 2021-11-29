ae573364104b5b65e037b118ca514150b3113f86
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-29 14:31:53 -0500

    test created

--- test/processJwt.test.js
+++ test/processJwt.test.js
@@ -4,9 +4,12 @@ const { processJwt } = require( '../index' );
 
 
 describe( 'ProcessJwt test', function () {
-    it( 'create the hash of string', function ( done ) {
-        // const test_hash = authUtil.createHmacString( "some data to hash" );
-        // assert.ok( test_hash, 'Hash not created' );
+    it( 'fromUrlSafeToBase64Jwt when called with url safe jwt returns base64 string', function ( done ) {
+        const urlSafeJwt = "eyJhbGciOiJzaGE1MTIiLCJ0eXAiOiJKV1QifQ.eyJpZCI6IjEyMzQ1Njc4OTAiLCJ0aW1lIjoxNjM4MjE0MDc0MDIzfQ.2t8UToQ6J-sIHUNH5XADFbdErSFeG8WEGj0FfR4YVnsQxnLJeDECtsNdj3IrMag2sNEEqqeuemycm1N70FrvHw";
+        const base64Jwt = "";
+
+        // const returnedBase64Jwt = processJwt.fromUrlSafeToBase64Jwt( urlSafeJwt );
+        // assert.deepStrictEqual( returnedBase64Jwt, base64Jwt );
         done();
     } );
 } );
