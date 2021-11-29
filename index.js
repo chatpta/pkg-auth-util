6451c079c6528ad72c3ebdcbd8924fcc6e2d84de
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-29 18:45:50 -0500

    old validate removed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
     AuthUtil: require( './lib/util/AuthUtil' ),
-    Validate: require( './lib/validate/validate' ),
     CommonUtil: require( './lib/util/commonUtil' ),
     Hash: require( './lib/util/hash' ),
     JwtCreator: require( './lib/util/jwtCreator' ),
