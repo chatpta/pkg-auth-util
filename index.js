43fd2ee7963b77959d19ebd0606ae29ace898964
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-29 14:18:55 -0500

    files created

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 module.exports = {
-    AuthUtil: require('./lib/util/AuthUtil'),
-    Validate: require('./lib/validate/validate'),
-    CommonUtil: require('./lib/util/commonUtil'),
-    Hash: require('./lib/util/hash'),
-    JwtCreator: require('./lib/util/jwtCreator'),
-    JwtReader: require('./lib/util/jwtReader'),
-    Middleware: require('./lib/middleware/middleware')
+    AuthUtil: require( './lib/util/AuthUtil' ),
+    Validate: require( './lib/validate/validate' ),
+    CommonUtil: require( './lib/util/commonUtil' ),
+    Hash: require( './lib/util/hash' ),
+    JwtCreator: require( './lib/util/jwtCreator' ),
+    JwtReader: require( './lib/util/jwtReader' ),
+    Middleware: require( './lib/middleware/middleware' ),
+    processJwt: require( './lib/processJwt' )
 }
---
+++ test/processJwt.test.js
@@ -0,0 +1,12 @@
+const assert = require( 'assert' ).strict;
+const { describe, it } = require( 'mocha' );
+const { processJwt } = require( '../index' );
+
+
+describe( 'ProcessJwt test', function () {
+    it( 'create the hash of string', function ( done ) {
+        // const test_hash = authUtil.createHmacString( "some data to hash" );
+        // assert.ok( test_hash, 'Hash not created' );
+        done();
+    } );
+} );
