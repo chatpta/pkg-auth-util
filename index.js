90ed9299f0ef7078a32bd45132a03cf485ad4b28
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-29 20:39:58 -0500

    auth util done for now

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 module.exports = {
-    AuthUtil: require( './lib/util/AuthUtil' ),
-    CommonUtil: require( './lib/util/commonUtil' ),
-    Hash: require( './lib/util/hash' ),
-    JwtCreator: require( './lib/util/jwtCreator' ),
-    JwtReader: require( './lib/util/jwtReader' ),
-    Middleware: require( './lib/middleware/middleware' ),
     stringUtilAuth: require( './lib/stringUtilAuth' ),
     cryptoUtilAuth: require( './lib/cryptoUtilAuth' ),
     jwtUtilAuth: require( './lib/jwtUtilAuth' )
