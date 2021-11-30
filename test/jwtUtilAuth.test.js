be6c85642ab3dbe89b875ec99a1788c8f5efc6ea
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-29 20:11:03 -0500

    jwt assemble and disassemble done

--- test/jwtUtilAuth.test.js
+++ test/jwtUtilAuth.test.js
@@ -13,4 +13,16 @@ describe( 'JwtUtilAuth test', function () {
         const returnedJwt = jwtUtilAuth.assembleJwt( header, payload, signature );
         assert.deepStrictEqual( returnedJwt, expectedJwt );
     } );
+
+    it( 'splitJwtInToHeaderPayloadSignature splits jwt into its parts', function () {
+        const jwt = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.-DprLrW2OyqiAFiuWs14WO2TWp2EHtaX7a63dqrklk-xrjaZMrcPhpX4hkZw803SQx5HpGc-7VYBX8l82XlMZg";
+        const headerExpected = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9";
+        const payloadExpected = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0";
+        const signatureExpected = "-DprLrW2OyqiAFiuWs14WO2TWp2EHtaX7a63dqrklk-xrjaZMrcPhpX4hkZw803SQx5HpGc-7VYBX8l82XlMZg";
+
+        const { header, payload, signature } = jwtUtilAuth.splitJwtInToHeaderPayloadSignature( jwt );
+        assert.deepStrictEqual( header, headerExpected );
+        assert.deepStrictEqual( payload, payloadExpected );
+        assert.deepStrictEqual( signature, signatureExpected );
+    } );
 } );
