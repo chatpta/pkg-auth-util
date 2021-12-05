b9e74c9a8156ff4f0b9e41c99fb3800eec572ed2
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-12-05 18:16:11 -0500

    version released 2.1.0

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const jwtLib = require( './lib/jwtLib' )
 const pwdUtilAuth = require( './lib/pwdUtilAuth' );
+const jwtUtilAuth = require( './lib/jwtUtilAuth' );
 
 module.exports = {
-    jwtUtilAuth: require( './lib/jwtUtilAuth' ),
+    jwtUtilAuth: {
+        createSignedJwtFromObject: jwtUtilAuth.createSignedJwtFromObject,
+        verifyJwtSignature: jwtUtilAuth.verifyJwtSignature,
+        getHeaderPayloadFromJwt: jwtUtilAuth.getHeaderPayloadFromJwt
+    },
+
     pwdUtilAuth: {
         createPasswordHashWithRandomSalt: pwdUtilAuth.createPasswordHashWithRandomSalt,
         createPasswordHashBasedOnSavedAlgorithmSalt: pwdUtilAuth.createPasswordHashBasedOnSavedAlgorithmSalt
     },
+
     stringUtilAuth: require( './lib/stringUtilAuth' ),
     jwtAsyncMiddleware: require( './lib/jwtAsyncMiddleware' ),
+
     jwtLib: {
         validateAndExtractJwtObject: jwtLib.validateAndExtractJwtObject,
         isJwtExpired: jwtLib.isJwtExpired,
