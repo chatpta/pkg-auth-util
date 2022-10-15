564e131530f2f1eb2460eef48faa7ff028c2e4bf
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2022-10-15 11:53:03 -0400

    Tests split into two files

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const assert = require( 'assert' ).strict;
 const { describe, it } = require( 'mocha' );
-const { jwtUtilAuth } = require( '../index' );
+const { jwtUtilAuth, pwdUtilAuth } = require( '../index' );
 const keys = require( './keys/keys' );
 
 
@@ -49,5 +49,28 @@ describe( 'JwtUtilAuth', function () {
 
         assert.deepStrictEqual( header, expectedHeader );
         assert.deepStrictEqual( payload, expectedPayload );
+    } );
+} );
+
+describe( 'PwdUtilAuth', function () {
+
+    it( 'createPasswordHashWithRandomSalt called with save hash', function () {
+        const password = "mySecretPassword";
+        const secret = 'bigSecret';
+        const algorithm = 'sha512';
+
+        const hash = pwdUtilAuth.createPasswordHashWithRandomSalt( password, secret, algorithm );
+
+        assert.deepStrictEqual( hash.length > 40, true );
+    } );
+
+    it( 'createPasswordHashBasedOnSavedAlgorithmSalt called with saved hash', function () {
+        const savedHash = "$1$c2hhNTEy$SOk/04Wn/ce1YIXHlUIqt5SgsuCCLIFjxpzHloVSxFh/z8JuLFshAaGNCkIRf47QSPCOJpkJ476N2eq1Yg1+yg==$6h29BnpUkqfrmtnY1xUrAGZcpcAl5cUEJ4Qjj+BGXbo=$";
+        const password = "mySecretPassword";
+        const secret = 'bigSecret';
+
+        const hash = pwdUtilAuth.createPasswordHashBasedOnSavedAlgorithmSalt( password, savedHash, secret );
+
+        assert.deepStrictEqual( hash, savedHash );
     } );
 } );
